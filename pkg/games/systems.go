077f04b63b77b320dbebefd7e5dc88dd50b8262a
Callan Barrett <callan@zoocar.org>
2022-09-14 13:15:35 +0800

    Define alternate cores

--- pkg/games/systems.go
+++ pkg/games/systems.go
@@ -11,14 +11,23 @@ type FileType struct {
 	Mgl        *MglParams
 }
 
+type AltRbfOpts map[string][]string
+
 type System struct {
 	Id        string
 	Name      string
 	Alias     []string
 	Folder    string
 	Rbf       string
+	AltRbf    AltRbfOpts
 	FileTypes []FileType
 }
+
+const (
+	AltRbfLLAPI   = "LLAPI"
+	AltRbfYC      = "YC"
+	AltRbfDualRAM = "DualRAM"
+)
 
 // First in list takes precendence for simple attributes in case there's a
 // conflict in the future.
@@ -29,7 +38,14 @@ var CoreGroups = map[string][]System{
 	"SMS":       {Systems["MasterSystem"], Systems["GameGear"]},
 }
 
-// TODO: launch game > launch new game same system > not working? should it?
+// FIXME: launch game > launch new game same system > not working? should it?
+// TODO: setname attribute
+// TODO: alternate cores
+// TODO: alternate arcade folders
+// TODO: custom scan function
+// TODO: custom launch function
+// TODO: support for multiple folders (think about symlink support here, check for dupes)
+// TODO: could cut down on work scanning by folder rather than system
 
 var Systems = map[string]System{
 	"Amiga": {
@@ -49,7 +65,6 @@ var Systems = map[string]System{
 		Id:     "Arcade",
 		Name:   "Arcade",
 		Folder: "_Arcade",
-		Rbf:    "",
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".mra"},
@@ -57,13 +72,15 @@ var Systems = map[string]System{
 			},
 		},
 	},
-	// TODO: support for multiple folders?
-	// TODO: could cut down on work scanning by folder rather than system
 	"Atari2600": {
 		Id:     "Atari2600",
 		Name:   "Atari 2600",
 		Folder: "ATARI7800",
 		Rbf:    "_Console/Atari7800",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"Atari7800_LLAPI"},
+			AltRbfYC:    []string{"Atari7800YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".a26"},
@@ -76,6 +93,9 @@ var Systems = map[string]System{
 		Name:   "Atari 5200",
 		Folder: "ATARI5200",
 		Rbf:    "_Console/Atari5200",
+		AltRbf: AltRbfOpts{
+			AltRbfYC: []string{"Atari5200YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".a52", ".car"},
@@ -89,6 +109,10 @@ var Systems = map[string]System{
 		Name:   "Atari 7800",
 		Folder: "ATARI7800",
 		Rbf:    "_Console/Atari7800",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"Atari7800_LLAPI"},
+			AltRbfYC:    []string{"Atari7800YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".a78", ".bin"},
@@ -121,6 +145,9 @@ var Systems = map[string]System{
 		Name:   "Commodore 64",
 		Folder: "C64",
 		Rbf:    "_Computer/C64",
+		AltRbf: AltRbfOpts{
+			AltRbfYC: []string{"C64YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".prg", ".crt", ".reu", ".tap"},
@@ -140,6 +167,9 @@ var Systems = map[string]System{
 		Alias:  []string{"Coleco"},
 		Folder: "Coleco",
 		Rbf:    "_Console/ColecoVision",
+		AltRbf: AltRbfOpts{
+			AltRbfYC: []string{"ColecoVisionYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".col", ".bin", ".rom", ".sg"},
@@ -157,6 +187,10 @@ var Systems = map[string]System{
 		Alias:  []string{"FamicomDiskSystem"},
 		Folder: "NES",
 		Rbf:    "_Console/NES",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"NES_LLAPI"},
+			AltRbfYC:    []string{"NESYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".fds"},
@@ -174,6 +208,10 @@ var Systems = map[string]System{
 		Alias:  []string{"GB"},
 		Folder: "GAMEBOY",
 		Rbf:    "_Console/Gameboy",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"Gameboy_LLAPI"},
+			AltRbfYC:    []string{"GameboyYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gb"},
@@ -191,6 +229,10 @@ var Systems = map[string]System{
 		Alias:  []string{"GBC"},
 		Folder: "GAMEBOY",
 		Rbf:    "_Console/Gameboy",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"Gameboy_LLAPI"},
+			AltRbfYC:    []string{"GameboyYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gbc"},
@@ -223,6 +265,10 @@ var Systems = map[string]System{
 		Name:   "Game Gear",
 		Folder: "SMS",
 		Rbf:    "_Console/SMS",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"SMS_LLAPI"},
+			AltRbfYC:    []string{"SMSYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gg"},
@@ -240,6 +286,10 @@ var Systems = map[string]System{
 		Alias:  []string{"GameboyAdvance"},
 		Folder: "GBA",
 		Rbf:    "_Console/GBA",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"GBA_LLAPI"},
+			AltRbfYC:    []string{"GBAYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gba"},
@@ -256,6 +306,9 @@ var Systems = map[string]System{
 		Name:   "GBA 2P",
 		Folder: "GBA2P",
 		Rbf:    "_Console/GBA2P",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"GBA2P_LLAPI"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gba"},
@@ -273,6 +326,10 @@ var Systems = map[string]System{
 		Alias:  []string{"MegaDrive"},
 		Folder: "Genesis",
 		Rbf:    "_Console/Genesis",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"Genesis_LLAPI"},
+			AltRbfYC:    []string{"GenesisYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".bin", ".gen", ".md"},
@@ -284,12 +341,17 @@ var Systems = map[string]System{
 			},
 		},
 	},
+	// TODO: Jaguar
 	"MegaCD": {
 		Id:     "MegaCD",
 		Name:   "Mega CD",
 		Alias:  []string{"SegaCD"},
 		Folder: "MegaCD",
 		Rbf:    "_Console/MegaCD",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"MegaCD_LLAPI"},
+			AltRbfYC:    []string{"MegaCDYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".cue", ".chd"},
@@ -307,6 +369,10 @@ var Systems = map[string]System{
 		Name:   "Neo Geo",
 		Folder: "NEOGEO",
 		Rbf:    "_Console/NeoGeo",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"NeoGeo_LLAPI"},
+			AltRbfYC:    []string{"NeoGeoYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".neo"},
@@ -332,6 +398,10 @@ var Systems = map[string]System{
 		Name:   "NES",
 		Folder: "NES",
 		Rbf:    "_Console/NES",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"NES_LLAPI"},
+			AltRbfYC:    []string{"NESYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".nes", ".nsf"},
@@ -349,6 +419,10 @@ var Systems = map[string]System{
 		Alias:  []string{"Playstation", "PS1"},
 		Folder: "PSX",
 		Rbf:    "_Console/PSX",
+		AltRbf: AltRbfOpts{
+			AltRbfYC:      []string{"PSXYC"},
+			AltRbfDualRAM: []string{"PSX_DualSDRAM"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".cue", ".chd"},
@@ -366,6 +440,10 @@ var Systems = map[string]System{
 		Alias:  []string{"S32X", "32X"},
 		Folder: "S32X",
 		Rbf:    "_Console/S32X",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"S32X_LLAPI"},
+			AltRbfYC:    []string{"S32XYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".32x"},
@@ -383,6 +461,10 @@ var Systems = map[string]System{
 		Alias:  []string{"SGB"},
 		Folder: "SGB",
 		Rbf:    "_Console/SGB",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"SGB_LLAPI"},
+			AltRbfYC:    []string{"SGBYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".gb", ".gbc"},
@@ -400,6 +482,10 @@ var Systems = map[string]System{
 		Alias:  []string{"SMS"},
 		Folder: "SMS",
 		Rbf:    "_Console/SMS",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"SMS_LLAPI"},
+			AltRbfYC:    []string{"SMSYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".sms", ".sg"},
@@ -411,12 +497,17 @@ var Systems = map[string]System{
 			},
 		},
 	},
+	// TODO: Saturn
 	"SNES": {
 		Id:     "SNES",
 		Name:   "SNES",
 		Alias:  []string{"SuperNintendo"},
 		Folder: "SNES",
 		Rbf:    "_Console/SNES",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"SNES_LLAPI"},
+			AltRbfYC:    []string{"SNESYC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".smc", ".sfc"},
@@ -434,6 +525,10 @@ var Systems = map[string]System{
 		Alias:  []string{"TGFX16", "PCEngine"},
 		Folder: "TGFX16",
 		Rbf:    "_Console/TurboGrafx16",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"TurboGrafx16_LLAPI"},
+			AltRbfYC:    []string{"TurboGrafx16YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".bin", ".pce"},
@@ -458,7 +553,11 @@ var Systems = map[string]System{
 		Name:   "TurboGraphx-16 CD",
 		Alias:  []string{"TGFX16-CD", "PCEngineCD"},
 		Folder: "TGFX16-CD",
-		Rbf:    "_Console/TGFX16",
+		Rbf:    "_Console/TurboGrafx16",
+		AltRbf: AltRbfOpts{
+			AltRbfLLAPI: []string{"TurboGrafx16_LLAPI"},
+			AltRbfYC:    []string{"TurboGrafx16YC"},
+		},
 		FileTypes: []FileType{
 			{
 				Extensions: []string{".cue", ".chd"},
