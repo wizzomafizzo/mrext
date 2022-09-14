2bd4c5635b1da7dbf7c4065f23b965b7ccae0d06
Callan Barrett <callan@zoocar.org>
2022-09-14 23:12:39 +0800

    Initial setup for user config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,4 +3,6 @@ package config
 const SdRoot = "/media/fat"
 const IndexName = "index"
 const CmdInterface = "/dev/MiSTer_cmd"
+
+// TODO: this can't be hardcoded if we want dynamic arcade folders
 const ArcadeCoresFolder = "/media/fat/_Arcade/cores"
