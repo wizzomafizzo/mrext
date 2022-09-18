c9498aed585d8047edbca600d741f08f150de241
Callan Barrett <callan@zoocar.org>
2022-09-18 22:48:54 +0800

    Mister ini read support

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,18 @@
 package config
 
+// TODO: should this be hardcoded? how common is usb0 setup?
 const SdFolder = "/media/fat"
 const CoreConfigFolder = SdFolder + "/config"
 
 const IndexName = "index"
 const ActiveGameFile = "/tmp/ACTIVEGAME"
+
+const MisterIniFile = SdFolder + "/MiSTer.ini"
+const MisterIniFile_Alt1 = SdFolder + "/MiSTer_alt_1.ini"
+const MisterIniFile_Alt2 = SdFolder + "/MiSTer_alt_2.ini"
+const MisterIniFile_Alt3 = SdFolder + "/MiSTer_alt_3.ini"
+
+const StartupFile = SdFolder + "/user-startup.sh"
 
 const CoreNameFile = "/tmp/CORENAME"
 const CurrentPathFile = "/tmp/CURRENTPATH"
@@ -16,6 +24,7 @@ const CmdInterface = "/dev/MiSTer_cmd"
 // TODO: this can't be hardcoded if we want dynamic arcade folders
 const ArcadeCoresFolder = "/media/fat/_Arcade/cores"
 
+// TODO: consider just hardcoding this in the paths list
 const GamesFolderSubfolder = "games"
 
 var GamesFolders = []string{
