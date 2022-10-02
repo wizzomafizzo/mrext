5b2d3d12492f7e57b6cb8003025013541d4dcd3f
Callan Barrett <callan@zoocar.org>
2022-10-02 08:32:36 +0800

    Initial setup for kb emulation

--- cmd/mm/main.go
+++ cmd/mm/main.go
@@ -4,15 +4,65 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/wizzomafizzo/mrext/pkg/games"
+	"github.com/wizzomafizzo/mrext/pkg/input"
 )
+
+func sendKeyboard(arg string) {
+	kb, err := input.NewKeyboard()
+	if err != nil {
+		fmt.Printf("error creating virtual keyboard: %s", err)
+		return
+	}
+	defer kb.Close()
+
+	var kbdMap = map[string]func(){
+		"close":            kb.Close,
+		"volumeup":         kb.VolumeUp,
+		"volumedown":       kb.VolumeDown,
+		"volumemute":       kb.VolumeMute,
+		"menu":             kb.Menu,
+		"back":             kb.Back,
+		"confirm":          kb.Confirm,
+		"cancel":           kb.Cancel,
+		"up":               kb.Up,
+		"down":             kb.Down,
+		"left":             kb.Left,
+		"right":            kb.Right,
+		"osd":              kb.Osd,
+		"coreselect":       kb.CoreSelect,
+		"screenshot":       kb.Screenshot,
+		"rawscreenshot":    kb.RawScreenshot,
+		"user":             kb.User,
+		"reset":            kb.Reset,
+		"pairbluetooth":    kb.PairBluetooth,
+		"changebackground": kb.ChangeBackground,
+		"togglecoredates":  kb.ToggleCoreDates,
+		"console":          kb.Console,
+		"computerosd":      kb.ComputerOsd,
+	}
+
+	if fn, ok := kbdMap[arg]; ok {
+		fn()
+	} else if i, err := strconv.Atoi(arg); err == nil {
+		err := kb.Device.KeyPress(i)
+		if err != nil {
+			fmt.Printf("error sending key: %s", err)
+		}
+	} else {
+		fmt.Printf("unknown keyboard command: %s", arg)
+		// TODO: print possible commands
+	}
+}
 
 func main() {
 	activePaths := flag.Bool("active-paths", false, "print active system paths")
 	allPaths := flag.Bool("all-paths", false, "print all detected system paths")
+	sendKb := flag.String("send-keyboard", "", "send keyboard command")
 	filterSystems := flag.String("s", "all", "restrict operation to systems (comma separated)")
 	timed := flag.Bool("t", false, "show how long operation took")
 	flag.Parse()
@@ -44,6 +94,8 @@ func main() {
 		for _, path := range paths {
 			fmt.Printf("%s:%s\n", path.System.Id, path.Path)
 		}
+	} else if *sendKb != "" {
+		sendKeyboard(*sendKb)
 	}
 
 	if *timed {
--- go.mod
+++ go.mod
@@ -10,11 +10,15 @@ require github.com/rthornton128/goncurses v0.0.0-20220628231859-fd57939296e5
 
 require golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
 
-require gopkg.in/ini.v1 v1.67.0
-
 require (
-	github.com/fsnotify/fsnotify v1.5.4
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad
+	github.com/stretchr/testify v1.8.0 // indirect
+	gopkg.in/ini.v1 v1.67.0
 )
 
+require github.com/fsnotify/fsnotify v1.5.4
+
+require golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+
 require golang.org/x/term v0.0.0-20220722155259-a9ba230a4035
+
+require github.com/bendahl/uinput v1.6.0
