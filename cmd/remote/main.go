55d290ccce42e1381e2a53e3cb15e9ad331dfd53
Callan Barrett <callan@zoocar.org>
2022-11-09 23:21:33 +0800

    Support serving react app from server

--- cmd/remote/main.go
+++ cmd/remote/main.go
@@ -1,52 +1,80 @@
 package main
 
 import (
-	"fmt"
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "App goes here.")
+//go:embed _client
+var client embed.FS
+
+func setupApi(subrouter *mux.Router) {
+	subrouter.HandleFunc("/screenshots", allScreenshots).Methods("GET")
+	subrouter.HandleFunc("/screenshots", takeScreenshot).Methods("POST")
+	subrouter.HandleFunc("/screenshots/{core}/{image}", viewScreenshot).Methods("GET")
+	subrouter.HandleFunc("/screenshots/{core}/{image}", deleteScreenshot).Methods("DELETE")
+
+	subrouter.HandleFunc("/systems", allSystems).Methods("GET")
+	subrouter.HandleFunc("/systems/{id}", launchCore).Methods("POST")
+
+	subrouter.HandleFunc("/wallpaper", allWallpapers).Methods("GET")
+	subrouter.HandleFunc("/wallpaper/{filename}", viewWallpaper).Methods("GET")
+	subrouter.HandleFunc("/wallpaper/{filename}", setWallpaper).Methods("POST")
+	subrouter.HandleFunc("/wallpaper/{filename}", deleteWallpaper).Methods("DELETE")
+
+	subrouter.HandleFunc("/music", musicStatus).Methods("GET")
+	subrouter.HandleFunc("/music/play", musicPlay).Methods("POST")
+	subrouter.HandleFunc("/music/stop", musicStop).Methods("POST")
+	subrouter.HandleFunc("/music/next", musicSkip).Methods("POST")
+	subrouter.HandleFunc("/music/playback/{playback}", setMusicPlayback).Methods("POST")
+	subrouter.HandleFunc("/music/playlist", musicPlaylists).Methods("GET")
+	subrouter.HandleFunc("/music/playlist/{playlist}", setMusicPlaylist).Methods("POST")
+
+	subrouter.HandleFunc("/games/search", searchGames).Methods("POST")
+	subrouter.HandleFunc("/games/launch", launchGame).Methods("POST")
+}
+
+func appHandler(rw http.ResponseWriter, req *http.Request) {
+	if !strings.HasPrefix(req.URL.Path, "/") {
+		req.URL.Path = "/" + req.URL.Path
+	}
+
+	build, err := fs.Sub(client, "_client/build")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filePath := strings.TrimLeft(path.Clean(req.URL.Path), "/")
+	if _, err := build.Open(filePath); err != nil {
+		req.URL.Path = "/"
+	}
+
+	http.FileServer(http.FS(build)).ServeHTTP(rw, req)
 }
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", home).Methods("GET")
+	setupApi(router.PathPrefix("/api").Subrouter())
 
-	router.HandleFunc("/screenshots", allScreenshots).Methods("GET")
-	router.HandleFunc("/screenshots", takeScreenshot).Methods("POST")
-	router.HandleFunc("/screenshots/{core}/{image}", viewScreenshot).Methods("GET")
-	router.HandleFunc("/screenshots/{core}/{image}", deleteScreenshot).Methods("DELETE")
+	router.PathPrefix("/").Handler(http.HandlerFunc(appHandler))
 
-	router.HandleFunc("/systems", allSystems).Methods("GET")
-	router.HandleFunc("/systems/{id}", launchCore).Methods("POST")
-
-	router.HandleFunc("/wallpaper", allWallpapers).Methods("GET")
-	router.HandleFunc("/wallpaper/{filename}", viewWallpaper).Methods("GET")
-	router.HandleFunc("/wallpaper/{filename}", setWallpaper).Methods("POST")
-	router.HandleFunc("/wallpaper/{filename}", deleteWallpaper).Methods("DELETE")
-
-	router.HandleFunc("/music", musicStatus).Methods("GET")
-	router.HandleFunc("/music/play", musicPlay).Methods("POST")
-	router.HandleFunc("/music/stop", musicStop).Methods("POST")
-	router.HandleFunc("/music/next", musicSkip).Methods("POST")
-	router.HandleFunc("/music/playback/{playback}", setMusicPlayback).Methods("POST")
-	router.HandleFunc("/music/playlist", musicPlaylists).Methods("GET")
-	router.HandleFunc("/music/playlist/{playlist}", setMusicPlaylist).Methods("POST")
-
-	router.HandleFunc("/games/search", searchGames).Methods("POST")
-	router.HandleFunc("/games/launch", launchGame).Methods("POST")
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PUT"},
+	})
 
 	srv := &http.Server{
-		// TODO: restrict this later
-		Handler:      cors.AllowAll().Handler(router),
-		Addr:         ":8000",
+		Handler:      cors.Handler(router),
+		Addr:         ":8182",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
