4ef4755bcc2f553ea97a294a470e1b3a8b94e6f5
Callan Barrett <callan@zoocar.org>
2023-05-23 08:56:11 +0800

    Disabled ini saving for now

--- cmd/remote/settings/ini.go
+++ cmd/remote/settings/ini.go
@@ -1,25 +1,24 @@
 package settings
 
 import (
-	"github.com/wizzomafizzo/mrext/pkg/mister"
 	"github.com/wizzomafizzo/mrext/pkg/service"
 	"net/http"
 )
 
 func HandleSaveIni(logger *service.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		iniFile, err := mister.LoadMisterIni()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			logger.Error("load mister.ini: %s", err)
-			return
-		}
-
-		err = mister.SaveMisterIni(iniFile)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			logger.Error("save mister.ini: %s", err)
-			return
-		}
+		//iniFile, err := mister.LoadMisterIni()
+		//if err != nil {
+		//	http.Error(w, err.Error(), http.StatusInternalServerError)
+		//	logger.Error("load mister.ini: %s", err)
+		//	return
+		//}
+		//
+		//err = mister.SaveMisterIni(iniFile)
+		//if err != nil {
+		//	http.Error(w, err.Error(), http.StatusInternalServerError)
+		//	logger.Error("save mister.ini: %s", err)
+		//	return
+		//}
 	}
 }
