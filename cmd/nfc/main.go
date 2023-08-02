d4e67e9bff9a5b1c3411e85cb3148bcbea202edb
Gareth Jones <me@gazj.co.uk>
2023-08-02 20:27:15 +0100

    Support reading core name from text on card

--- cmd/nfc/main.go
+++ cmd/nfc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"encoding/hex"
 	"fmt"
@@ -16,11 +17,6 @@ var (
 	nfcConnectionString = "" // Use the first NFC reader available. Be sure to configure /etc/nfc/libnfc.conf
 	supportedCardTypes  = []nfc.Modulation{
 		{Type: nfc.ISO14443a, BaudRate: nfc.Nbr106},
-		{Type: nfc.ISO14443b, BaudRate: nfc.Nbr106},
-		{Type: nfc.Felica, BaudRate: nfc.Nbr212},
-		{Type: nfc.Felica, BaudRate: nfc.Nbr424},
-		{Type: nfc.Jewel, BaudRate: nfc.Nbr106},
-		{Type: nfc.ISO14443biClass, BaudRate: nfc.Nbr106},
 	}
 	timesToPoll        = 20
 	periodBetweenPolls = 300 * time.Millisecond
@@ -60,12 +56,43 @@ func main() {
 			if currentCardID != lastSeenCardUID {
 				log.Println("New card UID: " + currentCardID)
 				lastSeenCardUID = currentCardID
-				loadCore(currentCardID)
+				var tagText = readTextRecord(pnd)
+				if tagText != "" {
+					log.Printf("Decoded text NDEF is: %s\n", tagText)
+					loadCoreFromFilename(tagText)
+					// TODO: if string is in special format e.g. !!GBA:abad9c764c35b8202e3d9e5915ca7007bdc7cc62 try to load that way.
+				} else {
+					log.Printf("No text NDEF found, falling back to UID mapping in CSV file")
+					loadCoreFromCardUID(currentCardID)
+				}
 			}
 		}
 
 		time.Sleep(periodBetweenLoop)
 	}
+}
+
+func readTextRecord(pnd nfc.Device) string {
+	blockCount := 35 // TODO: This is hardcoded for NTAG 213. needs to support N215 and N216
+	allBlocks := make([]byte, 0)
+	offset := 4
+	for i := 0; i <= (blockCount / 4); i++ {
+		blocks := readFourBlocks(pnd, byte(offset))
+		allBlocks = append(allBlocks, blocks...)
+		offset = offset + 4
+	}
+	log.Printf("Card hex: " + hex.EncodeToString(allBlocks))
+
+	// Find the text NDEF record
+	startIndex := bytes.Index(allBlocks, []byte{0x54, 0x02, 0x65, 0x6E})
+	endIndex := bytes.Index(allBlocks, []byte{0xFE})
+
+	if startIndex != -1 && endIndex != -1 {
+		tagText := string(allBlocks[startIndex+4 : endIndex])
+		return tagText
+	}
+
+	return "" // TODO: return error,string instead
 }
 
 func loadDatabase() {
@@ -81,28 +108,33 @@ func loadDatabase() {
 func readCsvFile(filePath string) [][]string {
 	f, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
+		log.Printf("Unable to load fallback database file: "+filePath, err)
 	}
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+		log.Fatal("CSV file appears to be badly formatted: "+filePath, err)
 	}
 
 	return records
 }
 
-func loadCore(cardId string) {
+func loadCoreFromFilename(filename string) {
+	var fullpath = "/media/fat/" + filename // TODO: saves a few chars on the tag but is it worth it?
+	log.Println("Loading core: " + fullpath)
+	mister.LaunchGenericFile(fullpath)
+}
+
+func loadCoreFromCardUID(cardId string) {
 	filename, ok := database[cardId]
 	if !ok {
 		log.Println("No core configured")
 		return
 	}
 
-	log.Println("Loading core: " + filename)
-	mister.LaunchGenericFile(filename)
+	loadCoreFromFilename(filename)
 }
 
 func getCardUID(target nfc.Target) string {
@@ -113,30 +145,24 @@ func getCardUID(target nfc.Target) string {
 		var ID = card.UID
 		UID = hex.EncodeToString(ID[:card.UIDLen])
 		break
-	case nfc.Modulation{Type: nfc.ISO14443b, BaudRate: nfc.Nbr106}:
-		var card = target.(*nfc.ISO14443bTarget)
-		UID = hex.EncodeToString(card.ApplicationData[:len(card.ApplicationData)])
-		break
-	case nfc.Modulation{Type: nfc.Felica, BaudRate: nfc.Nbr212}:
-		var card = target.(*nfc.FelicaTarget)
-		UID = hex.EncodeToString(card.ID[:card.Len])
-		break
-	case nfc.Modulation{Type: nfc.Felica, BaudRate: nfc.Nbr424}:
-		var card = target.(*nfc.FelicaTarget)
-		UID = hex.EncodeToString(card.ID[:card.Len])
-		break
-	case nfc.Modulation{Type: nfc.Jewel, BaudRate: nfc.Nbr106}:
-		var card = target.(*nfc.JewelTarget)
-		var ID = card.ID
-		UID = hex.EncodeToString(ID[:len(ID)])
-		break
-	case nfc.Modulation{Type: nfc.ISO14443biClass, BaudRate: nfc.Nbr106}:
-		var card = target.(*nfc.ISO14443biClassTarget)
-		var ID = card.UID
-		UID = hex.EncodeToString(ID[:len(ID)])
-		break
 	default:
 		log.Println("Unsupported card type :(")
 	}
 	return UID
+}
+
+func readFourBlocks(pnd nfc.Device, blockNumber byte) []byte {
+	// Read 16 bytes at a time from a Type 2 tag
+	// For NTAG this would be 4 blocks or pages.
+	tx := []byte{0x30, blockNumber}
+	rx := make([]byte, 16)
+
+	timeout := 0
+	_, err := pnd.InitiatorTransceiveBytes(tx, rx, timeout)
+	if err != nil {
+		fmt.Println("Error reading blocks: ", err)
+		return nil
+	}
+
+	return rx
 }
