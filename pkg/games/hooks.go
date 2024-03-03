d0d27556be62e5c1e6cd02933f9def5696d6c28e
Callan Barrett <callan@zoocar.org>
2024-03-03 13:32:30 +0800

    Fix wonderswan color launch issue

--- pkg/games/hooks.go
+++ pkg/games/hooks.go
@@ -8,46 +8,73 @@ import (
 	"github.com/wizzomafizzo/mrext/pkg/utils"
 )
 
-// Create the FDS folder and copy over the FDS BIOS from the NES folder if possible.
-func PreHookFDS(cfg *config.UserConfig) error {
-	const bootName = "boot0.rom"
+func CopySetnameBios(cfg *config.UserConfig, origSystem System, newSystem System, name string) error {
 	var biosPath string
 
-	nesSystem, err := GetSystem("NES")
-	if err != nil {
-		return err
-	}
-
-	for _, folder := range GetActiveSystemPaths(cfg, []System{*nesSystem}) {
-		checkPath := filepath.Join(folder.Path, bootName)
+	for _, folder := range GetActiveSystemPaths(cfg, []System{origSystem}) {
+		checkPath := filepath.Join(folder.Path, name)
 		if _, err := os.Stat(checkPath); err == nil {
 			biosPath = checkPath
 			break
 		}
 	}
 
-	if biosPath == "" {
+	if biosPath == "" || newSystem.SetName == "" {
 		return nil
 	}
 
-	fdsFolder, err := filepath.Abs(filepath.Join(filepath.Dir(biosPath), "..", "FDS"))
+	newFolder, err := filepath.Abs(filepath.Join(filepath.Dir(biosPath), "..", newSystem.SetName))
 	if err != nil {
 		return err
 	}
 
-	if _, err := os.Stat(filepath.Join(fdsFolder, bootName)); err == nil {
+	if _, err := os.Stat(filepath.Join(newFolder, name)); err == nil {
 		return nil
 	}
 
-	if err := os.MkdirAll(fdsFolder, 0755); err != nil {
+	if err := os.MkdirAll(newFolder, 0755); err != nil {
 		return err
 	}
 
-	return utils.CopyFile(biosPath, filepath.Join(fdsFolder, bootName))
+	return utils.CopyFile(biosPath, filepath.Join(newFolder, name))
+}
+
+func PreHookFDS(cfg *config.UserConfig) error {
+	nesSystem, err := GetSystem("NES")
+	if err != nil {
+		return err
+	}
+
+	fdsSystem, err := GetSystem("FDS")
+	if err != nil {
+		return err
+	}
+
+	return CopySetnameBios(cfg, *nesSystem, *fdsSystem, "boot0.rom")
+}
+
+func PreHookWSC(cfg *config.UserConfig) error {
+	wsSystem, err := GetSystem("WonderSwan")
+	if err != nil {
+		return err
+	}
+
+	wscSystem, err := GetSystem("WonderSwanColor")
+	if err != nil {
+		return err
+	}
+
+	err = CopySetnameBios(cfg, *wsSystem, *wscSystem, "boot.rom")
+	if err != nil {
+		return err
+	}
+
+	return CopySetnameBios(cfg, *wsSystem, *wscSystem, "boot1.rom")
 }
 
 var SystemPreHooks = map[string]func(*config.UserConfig) error{
-	"FDS": PreHookFDS,
+	"FDS":             PreHookFDS,
+	"WonderSwanColor": PreHookWSC,
 }
 
 func RunSystemPreHook(cfg *config.UserConfig, system System) error {
